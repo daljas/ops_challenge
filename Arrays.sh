68a984259ea9c1169ef36ef041065333313a7f7a
daljas <jasondallas52@gmail.com>
2023-10-26 20:59:07 -0400

    learned about for and do commands in bash

--- Arrays.sh
+++ Arrays.sh
@@ -1,26 +1,24 @@
-#!/bin/bash
 
 # Script Name:                  Arrays.sh
 # Author:                       Jason Dallas
 # Date of latest revision:      10/26/2023
-# Purpose:                      List name in a array
+# Purpose:                      Create directories and add text files to them
 
 # Declaration of variables
 dirs=("dir1" "dir2" "dir3" "dir4")
 text="Hoping this works."
 
-
 # Declaration of functions
-    
-Mkdir(){
-    mkdir "$dirs"
-    echo -e "$text" > "$dir/text.txt"
+Mkdir() {
+    for dir in "${dirs[@]}"; do
+        mkdir "$dir"
+        echo "$text" > "$dir/text.txt"
+    done
 }
 
-
-
 # Main
-Mkdir "${dirs[@]}"
+Mkdir
+
 
 
 
