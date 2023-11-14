e49f78bff5e220521cd6dca035040dda90dec903
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-11-14 16:22:51 -0500

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,11 @@
+# Import the Active Directory module (if not already loaded)
+Import-Module ActiveDirectory
+
+# Specify the username (samAccountName) of the user
+$userName = "YourUsername"
+
+# Get the user object
+$user = Get-ADUser -Identity $userName
+
+# Expire the user's password
+Set-ADUser -Identity $userName -ChangePasswordAtLogon $true
