064705d28bfe9bf32322c0003b53f976c3758ea5
Jason Dallas <146230423+daljas@users.noreply.github.com>
2023-11-14 16:35:14 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,11 +1,3 @@
-# Import the Active Directory module (if not already loaded)
-Import-Module ActiveDirectory
+wmic UserAccount where name='User' set Passwordexpires=true
 
-# Specify the username (samAccountName) of the user
-$userName = "User"
-
-# Get the user object
-$user = Get-ADUser -Identity $userName
-
-# Expire the user's password
-Set-ADUser -Identity $userName -ChangePasswordAtLogon $true
+net user "User" /logonpasswordchg:yes
